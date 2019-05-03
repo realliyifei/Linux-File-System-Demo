77c0698d530104831582e00bb6bae0a00ce10c4b
huxiao <hui.xiao.dk@gmail.com>
2019-05-02 21:11:47 -0500

    Create template for main.c , related .c files and headers for integration

---
+++ integration/headers/node.h
@@ -0,0 +1,9 @@
+#ifndef _NODEH_
+#define _NODEH_
+
+typedef struct node {
+	char *filepath;
+	struct node* next;
+} node;
+
+#endif
---
+++ integration/search.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "headers/search.h"
+#include "headers/linkedlist.h"
+
+node* search_by_name(char* token3, node* filelist)
+{
+	return NULL;
+}
+
+node* search_by_modification_time(char* token3, node* filelist)
+{
+	return NULL;
+}
+
+node* search_by_inode(char* token3, node* filelist)
+{
+	return NULL;
+}
