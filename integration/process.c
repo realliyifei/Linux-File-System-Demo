1d33cc78ec123fe98831a26faa0284328286b9c4
huxiao <hui.xiao.dk@gmail.com>
2019-05-08 18:38:00 -0500

    Remove mock data and fix typo

--- integration/process.c
+++ integration/process.c
@@ -109,28 +109,6 @@ node* process_criteria(char* token2, char* token3, node* filelist)
 		print_error_msg("Invalid Syntax: Criteria should be -name, -mmin or -inum");	/* criteria is not valid */
 	}
 
-    //Test : mock data
-    // node *dummy, *tail, *head;
-
-    // char *filepath0 = "testFolder0/testFile0";
-    // char *filepath1 = "testFolder0/testFolder1/testFile1";
-    // char *filepath2 = "testFolder0/testFolder1/testFile0";
-    // char *filepath3 = "testFolder0/testFolder1/test2";
-
-    // if(create_node(&dummy, "") == FAILURE)  
-    // {
-    //     return FAILURE;
-    // }           
-
-    // tail = dummy;                        
-    // add_node(&tail, filepath0);     
-    // add_node(&tail, filepath1);
-    // add_node(&tail, filepath2);
-    // add_node(&tail, filepath3);
-
-    // head = dummy->next;
-    // qualified_list = head;
-
 	return qualified_list;
 }
 
@@ -140,7 +118,7 @@ int process_delete(char* token4, char* token5, node* qualified_list)
 
 	if(strcmp(token4, "-delete") != 0 || strcmp(token5, "") != 0)
     {
-		print_error_msg("Invalid Input: For deleting files, enter -file after criteria");
+		print_error_msg("Invalid Input: For deleting files, enter -delete after criteria");
 		return FAILURE;
 	}
 
