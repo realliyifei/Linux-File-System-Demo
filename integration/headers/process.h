82360b50a9397dc6c094949f4c0fc63531fec56c
huxiao <hui.xiao.dk@gmail.com>
2019-05-07 22:46:00 -0500

    Add new header for process helper function

---
+++ integration/headers/process.h
@@ -0,0 +1,12 @@
+#ifndef _PROCESSH_
+#define _PROCESSH_
+
+#include "node.h"
+
+int process_find(char* token0);
+node* process_location(char* token1);
+node* process_criteria(char* token2, char* token3, node* filelist);
+int process_delete(char* token4, char* token5, node* qualified_list);
+
+#endif
+
