cd0c6e7fc016ea5853e3645b853b7376c6456b44
huxiao <hui.xiao.dk@gmail.com>
2019-05-08 18:36:54 -0500

    Integrate search.c

--- integration/search.c
+++ integration/search.c
@@ -1,13 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h> // stat function
+#include <unistd.h>   //   function
+#include <string.h>   // strchr, strcmp
+#include <time.h>
 #include "headers/search.h"
 #include "headers/linkedlist.h"
+#include "headers/constant.h"
+#include "headers/util.h"
 
-node* search_by_name(char* token3, node* filelist)
-{
-    return NULL;
-}
-
-node* search_by_modification_time(char* token3, node* filelist)
+/**
+ * Function: search_by_name
+ * @Exec Search all the files with name indicated by filelist and print the directory
+ * @Para1 name - the required file name to be searched
+ * @Para2 filelist - pointer to the head of the linkedlist of the filenames
+ * @Return pointer to the head of the linkedlist of the searched file
+ *          NULL if fails
+ */
+node* search_by_name(char* name, node* filelist)
 {
     DIR *dir;
 
@@ -73,7 +84,167 @@ node* search_by_modification_time(char* token3, node* filelist)
     return filelist_result->next;
 }
 
-node* search_by_inode(char* token3, node* filelist)
+/**
+ * Function: search_by_modification_time
+ * @Exec Search all the files with modification time mentioned by min 
+ *          Three conditions: less than, exactly equal to, and larger than 
+ * @Para1 min - the required modification time
+ * @Para2 filelist - pointer to the head of the linkedlist of the filenames
+ * @Return pointer to the head of the linkedlist of the searched file
+ *          NULL if fails
+ */
+node* search_by_modification_time(char* min, node* filelist)
 {
-    return NULL;
+    time_t t;
+    struct tm *timeinfo;
+    time(&t);
+    timeinfo = localtime(&t);
+    struct stat buf;
+    node *final_filelist;
+    node *tail;
+    node *temp = filelist;
+
+    int i_min; // = atoi(min);// make minutes in char to int
+
+    int result;
+    if (filelist == NULL)
+    {
+        //  printf("filelist is empty.\n");
+        return NULL;
+    }
+
+    create_node(&final_filelist, "");
+    tail = final_filelist;
+
+    // printf("creat new node\n");
+    if (min[0] == '+')
+    {
+        //test
+        printf("search.c min[0]+ -> before atoi token3: %s\n", min);
+        i_min = atoi((min) + 1);
+        printf("search.c min[0]+ -> after atoi token3: %s\n", min);
+        printf("minutes after '+': %d \n", i_min);
+
+        while ((temp->filepath) != NULL)
+        {
+
+            stat(temp->filepath, &buf);
+
+            printf("the filepath: %s\n", temp->filepath);
+            //printf("modified time %ld\n", (time(NULL) - buf.st_mtime));
+            printf("modified time: %s\n", ctime(&buf.st_mtime));
+            if ((time(NULL) - buf.st_mtime) >= (i_min * 60))
+            {
+                if (S_ISREG(buf.st_mode))
+                {
+                    add_node(&tail, temp->filepath);
+                }
+                // printf("added filepath'-': %s \n", temp->filepath);
+            }
+            //printf("not added filepath'-': %s \n", temp->filepath);
+
+            temp = temp->next;
+        }
+        printf("\n");
+        return final_filelist->next;
+    }
+
+    else if (min[0] == '-')
+    {
+        i_min = atoi((min) + 1);
+        printf("minutes after '-': %d \n", i_min);
+        while ((temp->filepath) != NULL)
+
+        {
+
+            result = stat(temp->filepath, &buf);
+            printf("the filepath: %s\n", temp->filepath);
+
+            printf("modified time: %s\n", ctime(&buf.st_mtime));
+            if ((time(NULL) - buf.st_mtime) < (i_min * 60))
+            {
+                add_node(&tail, temp->filepath);
+                printf("added filepath'-': %s \n", temp->filepath);
+            }
+
+            temp = temp->next;
+        }
+        printf("\n");
+        return final_filelist->next;
+    }
+
+    else
+    {
+        i_min = atoi(min);
+        while ((temp->filepath) != NULL)
+        {
+
+            result = stat(temp->filepath, &buf);
+            printf("the filepath: %s\n", temp->filepath);
+            printf("modified time %s\n", ctime(&buf.st_mtime));
+            if ((int)((time(NULL) - buf.st_mtime) / 60) == (i_min))
+            {
+                if (S_ISREG(buf.st_mode))
+                {
+                    add_node(&tail, temp->filepath);
+                }
+                // printf("filepath'-': %s \n", temp->filepath);
+            }
+            temp = temp->next;
+        }
+        printf("\n");
+        return final_filelist->next;
+    }
+}
+
+/**
+ * Function: search_by_inode
+ * @Exec Search all the files with i-node mentioned by inum
+ * @Para1 min - the required i-node
+ * @Para2 filelist - pointer to the head of the linkedlist of the filenames
+ * @Return pointer to the head of the linkedlist of the searched file
+ *          NULL if fails
+ */
+node* search_by_inode(char* inum, node* filelist)
+{
+    node *final_filelist;
+    node *temp = filelist;
+    node *tail;
+    // DIR *dir;
+
+    struct stat buf;
+    int result;
+    //struct dirent *ptr;
+    long int_num = atol(inum); // make minutes in char to int
+
+    if (filelist == NULL)
+    {
+        return NULL;
+    }
+    // else if ((dir = opendir(filelist->filepath)) == NULL)
+    // {
+    //     return NULL;
+    // }
+    create_node(&final_filelist, "");
+    tail = final_filelist;
+
+    while (temp != NULL)
+    {
+        //buf = malloc(sizeof(struct stat));
+        //ptr = readdir(dir);
+        //result =
+        stat(temp->filepath, &buf);
+        printf("the current file's inode is: %ld of %s \n", buf.st_ino, temp->filepath);
+        if ((long)(buf.st_ino) == int_num)
+        {
+            if (S_ISREG(buf.st_mode))
+            {
+                add_node(&tail, temp->filepath);
+            }
+        }
+        //free(buf);
+        temp = temp->next;
+    }
+    printf("\n");
+    return final_filelist->next;
 }
