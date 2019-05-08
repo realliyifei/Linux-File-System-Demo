6afb540c7017872fa3b7ba1747845b75f77f08e3
huxiao <hui.xiao.dk@gmail.com>
2019-05-07 23:47:15 -0500

    Add new declaration for integration

--- integration/headers/util.h
+++ integration/headers/util.h
@@ -4,6 +4,7 @@
 #include "node.h"
 int check_length(char user_input[]);
 int convert_user_instruction(char user_input[], char *tokens[]);
+void get_current_path(char* buffer, char* filename);
 void print_list(node* list);
 void print_error_msg(char* msg);
 
