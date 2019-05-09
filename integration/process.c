5e6d67c882d1dfc3dd619519af25bb3d34c0ddd6
Zhiming Zou <zhzou@mac02-cs.ms.okstate.edu>
2019-05-08 21:15:02 -0500

    impove the readability of process_location function, reducing nested if else and while statement

--- integration/process.c
+++ integration/process.c
@@ -26,63 +26,52 @@ node* process_location(char* token1)
     struct dirent *dir;// get detial file names in the stream
     node* filelist; // return a list of filename
     node* pointer;
-
+    
     filelist = NULL;
     pointer = NULL;
-
+    
     if(strcmp(token1,"") ==0)
     {
-		d = opendir(".");
+        d = opendir(".");
     }
     else
     {
-  		d = opendir(token1);
+        d = opendir(token1);
     }
-
-    if(d != NULL) // if the directory is found
-    {      
-        if(!create_node(&filelist, ""))
-        {
-            return NULL;
-        }
-        
-        dir = readdir(d);
-        
-        pointer = filelist;
-        
-        while(dir != NULL)
-        {
-            char current_path[MAX_TOKEN_LENGTH];
-            get_current_path(current_path, token1);
-
-            if(strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0 )
-            {
-                strcat(current_path, dir->d_name);
-                if(opendir(current_path) != NULL) // if the current dir->d_name is a new directory, then we have to deal with sub-directory
-                {
-                    pointer->next = process_location(current_path);
-                    while(pointer->next != NULL) // locate the tailer of the list
-                        pointer = pointer ->next;
-                }
-                else
-                {
-                     add_node(&pointer, current_path);
-                }
-                
-            }
-            
-            dir = readdir(d);
-        }
-        closedir(d);
-        return filelist->next;
-    }
-    else // if the directory is not exisitng, then return NULL
+    if(d == NULL) // if the directory is not found
     {
         print_error_msg("Invalid Input: Non-existing directory");
         return NULL;
-	}
-
-	return filelist->next;
+    }
+    if(!create_node(&filelist, ""))
+    {
+        return NULL;
+    }
+    pointer = filelist; // avoid changing head pointer(filelist)
+    while((dir=readdir(d))!= NULL)
+    {
+        char current_path[MAX_TOKEN_LENGTH];
+        get_current_path(current_path, token1);
+        
+        if(strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0 )
+        {
+            continue; //escape current dir and parent dir
+        }
+        
+        strcat(current_path, dir->d_name);
+        if(opendir(current_path) != NULL) // if the current dir->d_name is a new directory, then we have to deal with sub-directory
+        {
+            pointer->next = process_location(current_path);
+            while(pointer->next != NULL) // locate the tailer of the list
+                pointer = pointer ->next;
+        }
+        else
+        {
+            add_node(&pointer, current_path);
+        }
+    }
+    closedir(d);
+    return filelist->next;
 }
 
 node* process_criteria(char* token2, char* token3, node* filelist)
