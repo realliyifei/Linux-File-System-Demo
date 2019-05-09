5792fea0c83b5bb3e374923e3cf78d1ed873e54c
Fangda Guo <bug.freeday@gmail.com>
2019-05-09 16:42:36 -0500

    code standard on delect.lect.c , linkedlist.c , main.c , process.c . temp save

--- integration/main.c
+++ integration/main.c
@@ -8,70 +8,69 @@
 #include "headers/search.h"
 #include "headers/delete.h"
 
-				
 void prompt_input();
-int get_input(char* tokens[]);
-int process_input(char* tokens[]);
+int get_input(char *tokens[]);
+int process_input(char *tokens[]);
 
 void prompt_input()
 {
 	printf("Welcome to the virtual file system! \n");
-    printf("The VFS has four following functions \n");
-    printf("1. find where-to-look \n");
-    printf("Usage : \"find directory_name\" \n");
-    printf("2.   find where-to-look criteria \n");
-    printf(" -a. find where-to-look -name <specified name> \n");
-    printf(" -b. find where-to-look -mmin <specified number of minutes> \n");
-    printf(" -c. find where-to-look -inum <specified i-node number>) \n");
-    printf("3. find where-to-look criteria -delete \n");
-    printf("\n");
+	printf("The VFS has four following functions \n");
+	printf("1. find where-to-look \n");
+	printf("Usage : \"find directory_name\" \n");
+	printf("2.   find where-to-look criteria \n");
+	printf(" -a. find where-to-look -name <specified name> \n");
+	printf(" -b. find where-to-look -mmin <specified number of minutes> \n");
+	printf(" -c. find where-to-look -inum <specified i-node number>) \n");
+	printf("3. find where-to-look criteria -delete \n");
+	printf("\n");
 	printf("\n");
 }
 
-int get_input(char* tokens[])   
+int get_input(char *tokens[])
 {
 	int result;
 	char user_input[MAXTOKEN * MAX_TOKEN_LENGTH];
 
 	fflush(stdin);
-	
-	fgets(user_input, MAXTOKEN * MAX_TOKEN_LENGTH, stdin);   
 
-	if(!check_length(user_input) || !convert_user_instruction(user_input, tokens))
+	fgets(user_input, MAXTOKEN * MAX_TOKEN_LENGTH, stdin);
+
+	if (!check_length(user_input) || !convert_user_instruction(user_input, tokens))
 	{
 		return FAILURE;
-    }
+	}
 
-	 return SUCCESS;
+	return SUCCESS;
 }
 
-int process_input(char* tokens[])
-{	
+int process_input(char *tokens[])
+{
 	int result;
 	node *filelist, *qualified_list;
-	
-	filelist = NULL	;	
+
+	filelist = NULL;
 	qualified_list = NULL;
 
-	if(!process_find(tokens[0]) || (filelist = process_location(tokens[1])) == NULL)
+	if (!process_find(tokens[0]) || (filelist = process_location(tokens[1])) == NULL)
 	{
 		result = FAILURE;
 	}
-	else if(strcmp(tokens[2], "") == 0)
+	else if (strcmp(tokens[2], "") == 0)
 	{
 		print_list(filelist);
 		result = SUCCESS;
 	}
-	else if((qualified_list = process_criteria(tokens[2], tokens[3], filelist)) == NULL)
+	else if ((qualified_list = process_criteria(tokens[2], tokens[3], filelist)) == NULL)
 	{
 		result = FAILURE;
 	}
-	else if(strlen(tokens[4]) == 0)
+	else if (strlen(tokens[4]) == 0)
 	{
 		print_list(qualified_list);
 		result = SUCCESS;
 	}
-	else if(!process_delete(tokens[4], tokens[5], qualified_list))
+	else if (!process_delete(tokens[4], tokens[5], qualified_list))
 	{
 		result = FAILURE;
 	}
@@ -82,31 +81,30 @@ int process_input(char* tokens[])
 
 	free_list(filelist);
 	free_list(qualified_list);
-	
+
 	return result;
 }
-
 
 int main()
 {
 	int result;
-	char* tokens[MAXTOKEN];
+	char *tokens[MAXTOKEN];
 
-	for(int i = 0; i < MAXTOKEN; ++i)
+	for (int i = 0; i < MAXTOKEN; ++i)
 	{
-		tokens[i] = (char *) malloc(MAX_TOKEN_LENGTH);
+		tokens[i] = (char *)malloc(MAX_TOKEN_LENGTH);
 	}
 
 	prompt_input();
 
-	if(!get_input(tokens))
+	if (!get_input(tokens))
 	{
 		return FAILURE;
 	}
 
 	result = process_input(tokens);
-	
+
 	printf("%s\n", "Test: main ends");
-	
+
 	return result;
 }
--- integration/process.c
+++ integration/process.c
@@ -9,110 +9,109 @@
 #include "headers/search.h"
 #include "headers/delete.h"
 
-int process_find(char* token0)
+int process_find(char *token0)
 {
-	if(strcmp(token0, "find") != 0)
-	{
-    	print_error_msg("Invalid Input: Command should start with 'find' ");
-    	return FAILURE;
+    if (strcmp(token0, "find") != 0)
+    {
+        print_error_msg("Invalid Input: Command should start with 'find' ");
+        return FAILURE;
     }
 
     return SUCCESS;
 }
 
-node* process_location(char* token1)
+node *process_location(char *token1)
 {
-    DIR * d; // get dir stream
-    struct dirent *dir;// get detial file names in the stream
-    node* filelist; // return a list of filename
-    node* pointer;
-    
+    DIR *dir_stream;    // get dir stream
+    struct dirent *dir; // get detial file names in the stream
+    node *filelist;     // return a list of filename
+    node *pointer;
+
     filelist = NULL;
     pointer = NULL;
-    
-    if(strcmp(token1,"") ==0)
+
+    if (strcmp(token1, "") == 0)
     {
-        d = opendir(".");
+        dir_stream = opendir(".");
     }
     else
     {
-        d = opendir(token1);
+        dir_stream = opendir(token1);
     }
-    if(d == NULL) // if the directory is not found
+    if (dir_stream == NULL) // if the directory is not found
     {
         print_error_msg("Invalid Input: Non-existing directory");
         return NULL;
     }
-    if(!create_node(&filelist, ""))
+    if (!create_node(&filelist, ""))
     {
         return NULL;
     }
     pointer = filelist; // avoid changing head pointer(filelist)
-    while((dir=readdir(d))!= NULL)
+    while ((dir = readdir(dir_stream)) != NULL)
     {
         char current_path[MAX_TOKEN_LENGTH];
         get_current_path(current_path, token1);
-        
-        if(strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0 )
+
+        if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0)
         {
             continue; //escape current dir and parent dir
         }
-        
+
         strcat(current_path, dir->d_name);
-        if(opendir(current_path) != NULL) // if the current dir->d_name is a new directory, then we have to deal with sub-directory
+        if (opendir(current_path) != NULL) // if the current dir->d_name is a new directory, then we have to deal with sub-directory
         {
             pointer->next = process_location(current_path);
-            while(pointer->next != NULL) // locate the tailer of the list
-                pointer = pointer ->next;
+            while (pointer->next != NULL) // locate the tailer of the list
+                pointer = pointer->next;
         }
         else
         {
             add_node(&pointer, current_path);
         }
     }
-    closedir(d);
+    closedir(dir_stream);
     return filelist->next;
 }
 
-node* process_criteria(char* token2, char* token3, node* filelist)
+node *process_criteria(char *token2, char *token3, node *filelist)
 {
-	node* qualified_list;
+    node *qualified_list;
 
-	qualified_list = NULL;
+    qualified_list = NULL;
 
-	/* choose corresponding search function*/
-	if(strcmp(token2, "-name") == 0)
-	{
-		qualified_list = search_by_name(token3, filelist);
-	}
-	else if(strcmp(token2, "-mmin") == 0)
-	{
-		qualified_list = search_by_modification_time(token3, filelist);
-	}
-	else if(strcmp(token2, "-inum") == 0)
-	{
-		qualified_list = search_by_inode(token3, filelist);
-	}
-	else			
-	{
-		print_error_msg("Invalid Syntax: Criteria should be -name, -mmin or -inum");	/* criteria is not valid */
-	}
-
-	return qualified_list;
-}
-
-int process_delete(char* token4, char* token5, node* qualified_list)
-{
-	int result;
-
-	if(strcmp(token4, "-delete") != 0 || strcmp(token5, "") != 0)
+    /* choose corresponding search function*/
+    if (strcmp(token2, "-name") == 0)
     {
-		print_error_msg("Invalid Input: For deleting files, enter -delete after criteria");
-		return FAILURE;
-	}
+        qualified_list = search_by_name(token3, filelist);
+    }
+    else if (strcmp(token2, "-mmin") == 0)
+    {
+        qualified_list = search_by_modification_time(token3, filelist);
+    }
+    else if (strcmp(token2, "-inum") == 0)
+    {
+        qualified_list = search_by_inode(token3, filelist);
+    }
+    else
+    {
+        print_error_msg("Invalid Syntax: Criteria should be -name, -mmin or -inum"); /* criteria is not valid */
+    }
 
-	result = delete(qualified_list);
-
-	return result;
+    return qualified_list;
 }
 
+int process_delete(char *token4, char *token5, node *qualified_list)
+{
+    int result;
+
+    if (strcmp(token4, "-delete") != 0 || strcmp(token5, "") != 0)
+    {
+        print_error_msg("Invalid Input: For deleting files, enter -delete after criteria");
+        return FAILURE;
+    }
+
+    result = delete (qualified_list);
+
+    return result;
+}
